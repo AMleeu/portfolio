416fe2bb82c09b8fc964955733ea6000fb4698ce
AM leeu <albert.m.leeu@gmail.com>
2022-07-13 05:58:32 -0800

    updated styling of AsideSection component & introduced google fonts globally in App component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,45 @@
 <template>
   <div id="app">
-    <MainContainer/>
+    <MainContainer />
   </div>
 </template>
 
 <script>
-import MainContainer from "./components/MainContainer.vue"
+import MainContainer from "./components/MainContainer.vue";
 
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     MainContainer,
-  }
-}
+  },
+};
 </script>
 
 <style>
-:root{
-    --bg-black-900: #f2f2fc;
-    --bg-black-100: #fdf9ff;
-    --bg-black-50: #e8dfec;
-    --text-black-900: #302e4d;
-    --text-black-700: #504e70;
-    --text-margin-top:0.35em;
+@import url("https://fonts.googleapis.com/css2?family=Edu+TAS+Beginner:wght@500&family=Open+Sans:wght@300;500;600;800&display=swap");
+:root {
+  --bg-black-900: #f2f2fc;
+  --bg-black-100: #fdf9ff;
+  --bg-black-50: #cac7cc;
+  --text-black-900: #302e4d;
+  --text-black-700: #504e70;
+  --text-margin-top: 20px;
+  --primary: #ec1839;
 }
-*{
-    margin:0px;
-    padding:0px;
-    outline:none;
-    text-decoration:none;
-    box-sizing: border-box;  
+* {
+  margin: 0px;
+  padding: 0px;
+  outline: none;
+  text-decoration: none;
+  box-sizing: border-box;
 }
-body{
-    line-height:1.5;
-    font-size:16px;
+body {
+  line-height: 1.5;
+  font-size: 16px;
+  font-family: 'Open Sans', sans-serif;
 }
-::before, ::after{
-    box-sizing: border-box;
+::before,
+::after {
+  box-sizing: border-box;
 }
 </style>
--- src/components/AsideSection.vue
+++ src/components/AsideSection.vue
@@ -3,9 +3,12 @@
     <div id="logo">
       <a href="#"><span>A</span>lbert</a>
     </div>
-    <div class="nav">
+    <div class="nav-toggler">
+      <font-awesome-icon icon="fa-solid fa-bars" />
+    </div>
+    <ul class="nav">
       <li>
-        <a href="">
+        <a href="" class="active">
           <font-awesome-icon icon="fa-solid fa-home-alt" />
           &nbsp; home
         </a>
@@ -22,11 +25,10 @@
       </li>
       <li>
         <a href="">
-          <font-awesome-icon icon="fa-solid fa-phone-square-alt" /> &nbsp; Get
-          in Touch
+          <font-awesome-icon icon="fa-solid fa-fingerprint" /> &nbsp; reach me
         </a>
       </li>
-    </div>
+    </ul>
   </div>
 </template>
 
@@ -38,11 +40,12 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faHomeAlt,
   faUserGear,
-  faPhoneSquareAlt,
   faBoxOpen,
+  faBars,
+  faFingerprint
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faHomeAlt, faUserGear, faPhoneSquareAlt, faBoxOpen);
+library.add(faHomeAlt, faUserGear, faFingerprint, faBoxOpen, faBars);
 
 export default {};
 </script>
@@ -57,9 +60,108 @@ export default {};
   left: 0;
   height: 100%;
   z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right:1px solid var(--bg-black-50);
 }
-#aside-section li {
+/*=================================
+logo
+==================================*/
+#aside-section #logo{
+  position:absolute;
+  top:50px;
+  font-size: 30px;
+  text-transform: capitalize;
+}
+#aside-section #logo a{
+  color: var(--text-black-900);
+  font-weight: 600;
+  padding: 15px 20px;
+  font-size: 30px;
+  letter-spacing: 5px;
+  position: relative;
+}
+#aside-section #logo a::before{
+  content: '';
+  position: absolute;
+  width: 20px;
+  height:20px;
+  border-top:5px solid var(--primary);
+  border-left:5px solid var(--primary);
+  top:0;
+  left:0;
+}
+#aside-section #logo a::after{
+  content: '';
+  position: absolute;
+  width: 20px;
+  height:20px;
+  border-bottom:5px solid var(--primary);
+  border-right:5px solid var(--primary);
+  bottom:0;
+  right:0;
+}
+#aside-section #logo span{
+  font-family: 'Edu TAS Beginner', cursive;
+}
+
+/*=================================
+nav-toggler
+==================================*/
+#aside-section .nav-toggler{
+  height:40px;
+  width: 45px;
+  border: 1px solid var(--bg-black-50);
+  cursor:pointer;
+  position:fixed; 
+  left:300px;
+  top: 20px;
+  border-radius: 5px;
+  background: var(--bg-black-100);
+  display:none;
+  justify-content: center;
+  align-items: center;
+}
+#aside-section .nav-toggler .svg-inline--fa{
+  color:var(--primary);
+}
+
+/*=================================
+nav
+==================================*/
+#aside-section .nav{
   list-style: none;
+  margin-top: 50px;
+}
+#aside-section .nav li{
   margin-top: var(--text-margin-top);
+  display:block;
+}
+#aside-section .nav li a{
+  font-size:16px;
+  font-weight: 600;
+  display:block;
+  border-bottom: 1px solid var(--bg-black-50);
+  color:var(--text-black-900);
+  padding: 5px 15px;
+}
+#aside-section .nav li a:hover,
+#aside-section .nav li a:focus{
+  color:var(--bg-black-50);
+}
+#aside-section .nav a.active{
+  color: var(--primary);
+}
+#aside-section .nav li a.active:hover,
+#aside-section .nav li a.active:focus{
+  color:var(--primary);
+  cursor:default;
+}
+/*
+  give each icon a standard width
+*/
+#aside-section li .svg-inline--fa{
+  width:18px;
 }
 </style>
